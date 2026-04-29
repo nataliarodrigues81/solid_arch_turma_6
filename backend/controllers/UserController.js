fd9e9a3eaad0166828b62bab032bd80c177f0043
nataliarodrigues81 <rodrigueznati2006@gmail.com>
2026-04-29 12:45:25 -0300

    Aula29/04 - Liskowski em validation-token

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,6 +1,8 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const createUserToken = require('../helpers/create-user-token')
+const getToken = require('../helpers/get-token')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -91,6 +93,45 @@ module.exports = class UserController {
         }
 
         await createUserToken(userExists, req, res)
+    }
+
+    static async checkUser(req, res){
+        let currentUser
+
+        console.log(req.headers.authorization)
+
+        if(req.headers.authorization) {
+            const token = getToken(req)
+            const decoded = jwt.verify(token, 'fatec-turma6-a2026')
+
+            currentUser = await User.findById(decoded.id)
+            currentUser.password = undefined
+        }else{
+            currentUser = null
+        }
+
+        res.status(200).send(currentUser)
+    }
+
+    static async getUserById(req, res){
+        const id = req.params.id 
+
+        const user = await User.findById(id)
+
+        if(!user){
+            res.status(404).json({
+                message: 'Usuário não encontrado'
+            })
+            return
+        }
+
+        res.status(200).json({user})
+    }
+
+    static async editUser(req, res){
+        res.status(200).json({
+            message: 'Usuário atualizado com sucesso'
+        })
     }
 }
 
