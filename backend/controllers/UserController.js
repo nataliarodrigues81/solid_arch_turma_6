6f5fe7194e0c1c13e60986ec0cf16c5fa4d420e7
nataliarodrigues81 <rodrigueznati2006@gmail.com>
2026-05-04 09:25:30 -0300

    Aula 05/04 Edit Update

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const createUserToken = require('../helpers/create-user-token')
 const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -128,11 +129,60 @@ module.exports = class UserController {
         res.status(200).json({user})
     }
 
+    
     static async editUser(req, res){
-        res.status(200).json({
-            message: 'Usuário atualizado com sucesso'
-        })
+        const id = req.params.id 
+
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+
+
+        const {name, email, phone, password, confirmpassword} = req.body
+        let image = ''
+        if(!name){
+            res.status(422).json({ message: 'Nome é obrigatório'})
+            return
+        }
+
+        if(!email){
+            res.status(422).json({ message: 'Email é obrigatório'})
+            return
+        }
+
+        if(!phone){
+            res.status(422).json({ message: 'Telefone é obrigatório'})
+            return
+        }
+
+        if(!password){
+            res.status(422).json({ message: 'Senha é obrigatória'})
+            return
+        }
+
+        if(!confirmpassword){
+            res.status(422).json({ message: 'Confirmação de senha é obrigatória'})
+            return
+        }
+
+        if(password !== confirmpassword){
+            res.status(422).json({ message: 'As senhas não coincidem'})
+            return
+        }
+
+        const userExists = await User.findOne({ email: email})
+
+        if(userExists.email === email && userExists){
+            res.status(422).json({message: 'Existe um problema de chave e-mail com a edição.'})
+            return
+        }
+
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
+
     }
+
+
 }
 
 
---
+++ backend/helpers/get-user-by-token.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
+const getUserToken = async (token) => {
+    const decoded = jwt.verify(token, 'fatec-turma6-a2026')
+    const userId = decoded.id 
+    const user = await User.findById({_id: userId})
+    return user
+}
+
+module.exports = getUserToken
